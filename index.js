03996afcfe9f691a30649201010b5aa7898855a3
unknown <migueldude27@gmail.com>
2023-03-22 14:37:30 -0400

    modified again

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Imports needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("generateMarkdown.js");
+const generateMarkdown = require("markDown.js");
 
 // Array of questions for user to input
 const questions = [
